ba5fc282e6130288c1ce2a8d072c7f0a1d0612f5
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-08-08 17:14:47 +0530

    Without class sed lipe

--- MandiData.py
+++ MandiData.py
@@ -2,18 +2,12 @@ import requests
 from pprint import pprint
 
 apikey = "579b464db66ec23bdd000001d18f8b46035d4f0373893279d670e4fd"
-class MandiData:
-
-    def __init__(self, api_key, date = None):
-        self.apiKey = api_key
-        self.date = date
-        self.baseUrl = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070"
-        self.params = {"api-key":  self.apiKey, "limit" : 500, "date":self.date, "format":"json"}
+baseUrl = "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070"
+params = {"api-key":  apiKey, "limit" : 500,"format":"json"}
     
+def getData():
+    response = requests.get(baseUrl,params)
+    response = response.json()
+    return response
 
-    def getData(self):
-        response = requests.get(self.baseUrl, self.params)
-        response = response.json()
-        return response
-
-pprint(MandiData(apikey).getData())
+pprint(getData())
