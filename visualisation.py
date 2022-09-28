3085b1a026eb9a0c845641b087b421f2b1cdfa34
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-09-28 22:03:56 +0530

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,2 +1,19 @@
-import fpdf
-import geopy
+from geopy.geocoders import Nominatim
+import geopy.distance as dist
+import tkintermapview
+
+def get_long_lat(location):
+    geolocator = Nominatim(user_agent="cpm")
+    convert_location = geolocator.geocode(location)
+    return convert_location.longitude, convert_location.latitude
+
+def get_distance(location_1,location_2):
+    return dist.distance(location_1,location_2).km
+
+def shortest_dist(distances):
+    buffer_list = []
+    for i in distances:
+        buffer_list.append(i[2])
+    min_dist = min(buffer_list)
+    index = buffer_list.find(min_dist)
+    return distances[index]
