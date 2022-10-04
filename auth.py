f7cefc09f6fbcf5b3b5700ad0541ad92cb784d68
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-10-04 18:09:22 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,39 @@
+import pickle
+import mysql.connector as mc
+
+def auth(user_entry, pass_entry):
+    user = user_entry.get()
+    pswd = pass_entry.get()
+    if user == "":
+        user = "root"
+    else:
+        pass
+    if pswd == "":
+        return False 
+    else:
+        pass
+    data = open("secure/auth.dat", "wb")
+    auth_data = [user,pswd]
+    pickle.dump(auth_data, data)
+    data.close()
+    try:
+        con = mc.connect(user=user,host="localhost",password=pswd)
+        return con.is_connected()
+    except:
+
+        return False
+
+
+def get_auth():
+    with open("secure/auth.dat","rb") as auth:
+        data = pickle.load(auth)
+        user = data[0]
+        pswd = data[1]
+    return user, pswd
+
+    
+    
+
+
+
+
---
+++ dbms.py
@@ -0,0 +1,127 @@
+import mysql.connector as mc
+import MandiData as md
+import pickle
+import os
+from datetime import date
+import auth 
+today_ = str(date.today())
+today_ = today_.split("-")
+today_ = today_[2] +"/"+ today_[1] +"/"+ today_[0]
+
+user, pswd = auth.get_auth()
+def establishCon():
+    con = mc.connect(host="localhost",user=user,password=pswd, database="cpm")
+    return con
+
+def checkCon(connection):
+    try:
+        if connection.is_connected():
+            createTable()
+            return True
+        else:
+            return False
+    except:
+        con = mc.connect(host="localhost",user=user,password=pswd)
+        cur=con.cursor()
+        cur.execute("create database cpm")
+        cur.commit()
+        con.close()
+        establishCon()
+        createTable()
+
+
+
+def createTable():
+    try:
+        con=establishCon()
+        cur = con.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS cpm_data(arrival_date	VARCHAR(10) NOT NULL,commodity	VARCHAR(50) NOT NULL ,district VARCHAR(50) NOT NULL,market	VARCHAR(50) NOT NULL,max_price	FLOAT NOT NULL,min_price VARCHAR(10) NOT NULL,modal_pricE VARCHAR(10) NOT NULL,state	VARCHAR(50) NOT NULL,variety VARCHAR(50) NOT NULL)')
+        con.commit()
+        establishCon().close()
+    except:
+        return None
+
+
+
+def insertData(data):
+    date =data["arrival_date"]
+    commodity=data["commodity"]
+    district = data["district"]
+    market = data["market"]
+    max_price = data["max_price"]
+    min_price =data["min_price"]
+    modal_price = data["modal_price"]
+    state = data["state"]
+    variety = data["variety"]
+    con=establishCon()
+    cur=con.cursor()
+    cur.execute(f'INSERT INTO cpm_data VALUES("{date}", "{commodity}", "{district}", "{market}", {float(max_price)}, {min_price}, {modal_price}, "{state}", "{variety}")')
+    con.commit()
+    con.close()
+
+
+
+
+def getDbData():
+    con=establishCon()
+    cur=con.cursor()
+    cur.execute("select * from cpm_data")
+    data = cur.fetchall()
+    return data
+
+
+def getParticularData(commodity, date = today_):
+    con = establishCon()
+    cur = con.cursor()
+    cur.execute(f'select * from cpm_data where commodity = "{commodity}" and arrival_date = "{date}"')
+    result_set = cur.fetchall()
+    return result_set
+
+def get_max_price_cmd(date, commodity):
+    con = establishCon()
+    cur = con.cursor()
+    command = f'select max_price , market from cpm_data where commodity="{commodity}" and arrival_date="{date}"order by max_price desc limit 1'
+    cur.execute(command)
+    result = cur.fetchall()
+    return result[0]
+
+
+def delete_old(date):
+    con = establishCon()
+    cur = con.cursor()
+    command = f'DELETE FROM cpm_data WHERE arrival_date != "{date}"'
+    try:
+        cur.execute(command)
+        con.commit()
+    except:
+        pass
+    con.close()
+def save_comodity_name():
+    data_pack = md.commodity_name_list()
+    data_pickle = open("secure/preLoader_data.dat", "wb")
+    pickle.dump(data_pack, data_pickle)
+    data_pickle.close()
+
+
+def load_pickle_data():
+    if os.path.exists('secure/preLoader_data.dat'):
+        with open("secure/preLoader_data.dat", 'rb') as data:
+            data_set = pickle.load(data)
+        
+    else:
+        save_comodity_name()
+        with open("secure/preLoader_data.dat", 'rb') as data:
+            data_set = pickle.load(data)
+    
+    result = []
+    for i in data_set:
+        result.append(i["commodity"])
+    output = []
+    for x in result:
+        if x not in output:
+            output.append(x)
+    output.sort()
+    return output
+
+checkCon(establishCon())
+
