729c426ae764d0926021d488f844e11eb59cc38e
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-10-16 14:04:07 +0530

    Update setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -11,21 +11,12 @@ timeout = 5
 
 tk.Label(root, text = "please wait untill setup finishes").pack(padx=10,pady=10)
 def setup_fn():    
-    tk.Label(root, text="installation started").pack(padx=10,pady=10)
     os.system("pip install geopy")
-    tk.Label(root, text="geopy installed ...").pack(padx=10, pady=10)
     os.system("pip install customtkinter")
-    tk.Label(root, text="customtkinter installed ...").pack(padx=10,pady=10)
     os.system("pip install tkintermapview")
-    tk.Label(root, text="Tkintermapview installed ...").pack(padx=10,pady=10)
     os.system("pip install cachetools")
-    tk.Label(root, text="cachetools installed ...").pack(padx=10,pady=10)
     os.system("pip install mysql-connector")
-    tk.Label(root, text="mysql-connector installed ...").pack(padx=10,pady=10)
     os.system("pip install fpdf2")
-    tk.Label(root, text="fpdf2 installed ...").pack(padx=10,pady=10)
-    tk.Label(root, text="INstallation completed").pack(padx=10,pady=10)
-    tk.Label(root, text="closing in 3 sec").pack(padx=10,pady=10)
     time.sleep(3)
     root.destroy()
 
