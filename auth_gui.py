7d44a20c1c8719d8a176327f508c3a0a5ef61a9c
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-11-21 23:00:07 +0530

    Update auth_gui.py

--- auth_gui.py
+++ auth_gui.py
@@ -7,7 +7,7 @@ ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 auth_window = ctk.CTk()
 auth_window.title("Commodity price manager")
-auth_window.geometry("300x400")
+auth_window.geometry("350x400")
 
 auth_window.iconbitmap("resources/favicon.ico")
 def ok():
@@ -24,6 +24,23 @@ def error():
     ok_btn = ctk.CTkButton(master=pop_frame,text="ok",image=error_img,hover_color="gray25",compound="right",command=ok)
     ok_btn.pack(padx=20,pady=20)
 
+def show_pswd():
+    global pswd_state,closed_eye,open_eye,show_pass
+    if pswd_state != True:
+        pswd_state = True
+        
+        pass_entry.configure(show="")
+        show_pass.configure(image=closed_eye)
+
+    else:
+        pswd_state=False
+        pass_entry.configure(show="#")
+        show_pass.configure(image=open_eye)
+
+    
+
+
+
 def call_auth():
     auth_ = auth.auth(user_entry,pass_entry)
     if auth_:
@@ -36,6 +53,7 @@ def call_auth():
         pass_entry.delete(0,tk.END)
         error()
 
+pswd_state = False
 auth_frame = ctk.CTkFrame(master = auth_window)
 auth_frame.pack(padx=50,pady=50)
 user_label = tk.Label(auth_frame, text="Enter user name", bg="#292929", fg="white")
@@ -44,9 +62,13 @@ user_entry = ctk.CTkEntry(master=auth_frame,placeholder_text="Enter username")
 user_entry.pack(padx=20,pady=10,anchor="w")
 pass_label = tk.Label(auth_frame, text="Enter password", bg="#292929",fg="white")
 pass_label.pack(padx=20,pady=10,anchor="w")
-pass_entry = ctk.CTkEntry(master=auth_frame,placeholder_text="Enter password")
+pass_entry = ctk.CTkEntry(master=auth_frame,placeholder_text="Enter password", show="#")
 pass_entry.pack(padx=20,pady=10,anchor="w")
+open_eye = ImageTk.PhotoImage(Image.open("resources\open_eye.png").resize((20, 20)))
+closed_eye = ImageTk.PhotoImage(Image.open("resources\eye-closed.png").resize((20, 20)))
+show_pass = ctk.CTkButton(master=auth_frame,text="", image=open_eye,compound="top", hover_color="gray25",width=40,command=show_pswd)
+show_pass.pack(padx=10,pady=10,side=tk.RIGHT)
 auth_img = ImageTk.PhotoImage(Image.open("resources/auth.png").resize((20, 20)))
-login_btn = ctk.CTkButton(master=auth_frame, text="Authenticate", command=call_auth,image=auth_img,compound="right", width=40,height=40,hover_color="gray25")
+login_btn = ctk.CTkButton(master=auth_frame, text="Authenticate", command=call_auth,image=auth_img,compound="right", width=60,height=40,hover_color="gray25")
 login_btn.pack(padx=20,pady=10,anchor="c")
 auth_window.mainloop()
