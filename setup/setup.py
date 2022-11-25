4cf4808501e595bc4f9a721ec857347ada7d3bf0
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-11-25 21:33:50 +0530

    Update setup.py

--- setup/setup.py
+++ setup/setup.py
@@ -18,6 +18,7 @@ def setup_fn():
     os.system("pip install cachetools")
     os.system("pip install mysql-connector-python")
     os.system("pip install fpdf2")
+    os.system("pip install tkvideo")
     time.sleep(3)
     root.destroy()
 
